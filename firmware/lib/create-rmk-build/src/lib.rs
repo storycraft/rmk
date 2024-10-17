858638b2fc988a54556c4ef9219204945827c765
storycraft <storycraft@pancake.sh>
2024-10-17 20:52:09 +0900

    feat: add doc

--- firmware/lib/create-rmk-build/src/lib.rs
+++ firmware/lib/create-rmk-build/src/lib.rs
@@ -2,6 +2,7 @@ use std::{env, error::Error, fs, path::PathBuf};
 
 use usb_keyboard_config::build_config;
 
+/// Create keyboard configuration source by reading configuration toml
 pub fn define_keyboard_config(path: &str, out_name: &str) -> Result<(), Box<dyn Error>> {
     println!("cargo::rerun-if-changed={path}");
 
